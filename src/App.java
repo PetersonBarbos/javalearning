40d1132c632321c98c8ace02b24616c1168839f9
PetersonBarbos <128330730+PetersonBarbos@users.noreply.github.com>
2023-08-28 04:53:57 -0300

    .

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    
+    }
+}
+/*Eu tenho um método principal(main) que está dentro da classe app que faz parte do pacote app, as classes são compostas de métodos e pode ter várias classes dentro do pacote */
+// a primeira letra maiúscula e o resto da palavra em camelcase ou é classe ou interface
+//ex: PrimeiroPrograma, AlunosCursoEmVideo
+//se a primeira letra for minúscula e o resto em camelCase pode ser um atributo, um método, ou uma variável
+//ex Atributo: nomeAluno; ex variável: mediaPrimeiroBimestre; ex método: lancarNotaAluno
+// se for todas letras minúsculas é um pacote
+// ex pacote: alunoscursoemvideo
+//se for tudo maiúsculo é uma constante
+//ex constante: VALOR_DE_PI
